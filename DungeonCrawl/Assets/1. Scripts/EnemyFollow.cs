54748bd28c7549ada220827a371d662f2f66de56
DirkvdL <fritsdirk@hotmail.nl>
2020-01-07 20:24:14 +0100

    Initial

---
+++ DungeonCrawl/Assets/1. Scripts/EnemyFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    Ray enemyRay;
+    public Color rayColor;
+    RaycastHit rayHit;
+    bool follow;
+
+    private NavMeshAgent agent;
+    public GameObject getHim;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        enemyRay = new Ray(transform.position, -transform.position * 10);
+        Debug.DrawRay(transform.position, -transform.position * 10, rayColor);
+
+        if (Physics.Raycast(transform.position, -transform.position, 10))
+        {
+            follow = true;
+        }
+
+        if (follow == true)
+        {
+            agent.SetDestination(getHim.transform.position); 
+        }
+    }
+
+}
---
+++ DungeonCrawl/Assets/1. Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float normalspeed = 0.0f;
+
+    private Rigidbody player;
+
+    private float horizontal;
+    private float vertical;
+
+    private void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal") * normalspeed * Time.deltaTime;
+        vertical = Input.GetAxis("Vertical") * normalspeed * Time.deltaTime;
+        transform.Translate(horizontal, 0, vertical);
+    }
+}
