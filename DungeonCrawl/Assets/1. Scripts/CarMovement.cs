3c8f9fc2266e1916430e0b5520e9795eb9401fcf
Poeiesz <51441266+Poeiesz@users.noreply.github.com>
2020-01-07 21:00:58 +0100

    yes
    movement yolo

---
+++ DungeonCrawl/Assets/1. Scripts/CarMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public int speed;
+    public int rotationspeed;
+    void Update()
+    {
+        //if (Input.GetKey("w"))
+        //{
+        //    Vector3 position = this.transform.position;
+        //    position.z++;
+        //    this.transform.position = position;
+        //}
+        //if (Input.GetKey("s"))
+        //{
+        //    Vector3 position = this.transform.position;
+        //    position.z--;
+        //    this.transform.position = position;
+        //}
+        //if (Input.GetKey("d"))
+        //{
+        //    Vector3 position = this.transform.position;
+        //    position.x++;
+        //    this.transform.position = position;
+        //}
+        //if (Input.GetKey("a"))
+        //{
+        //    Vector3 position = this.transform.position;
+        //    position.x--;
+        //    this.transform.position = position;
+        //}
+
+        transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Rotate (0, Input.GetAxis("Horizontal") * rotationspeed * Time.deltaTime, 0);
+    }
+}
